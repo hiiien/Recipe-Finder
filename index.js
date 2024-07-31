ccbac29730d3f6226f8b41ede89bb4633ad22dc1
Gavin Ward <gavin.gbw@gmail.com>
2024-07-31 15:21:50 -0500

    add homepage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("home.ejs");
 })
 
 app.post("/recipes", async (req, res) => {
